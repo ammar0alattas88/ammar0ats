5b540f2c98938377c906e74aa2e05953aba88281
ammar0alattas88 <alattasammar728@gmail.com>
2026-07-26 18:15:32 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+﻿const CACHE_NAME = 'sana-cache-v3';
+const ASSETS = [
+  './',
+  './ط¹ظ…ط§ط±.html',
+  './manifest.json',
+  './ط³ظ†ط§ 2.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
+  );
+});
